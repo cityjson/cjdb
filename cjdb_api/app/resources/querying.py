e43f8cf0c2c7594195f2d2a70e168926dd4a17fe
siebren014 <74716099+siebren014@users.noreply.github.com>
2022-10-06 17:43:35 +0200

    FilterAttributes

--- cjdb_api/app/resources/querying.py
+++ cjdb_api/app/resources/querying.py
@@ -2,10 +2,13 @@ from flask_restful import Resource
 from model.sqlalchemy_models import CjObjectModel
 from cjdb_api.app.schemas import CityJsonSchema
 from cjdb_api.app.db import session, engine
+from sqlalchemy.sql import text
 cityjson_schema = CityJsonSchema()
 cityjson_list_schema = CityJsonSchema(many=True)
 
 ##working
+
+#Select all objects in the database
 class all(Resource):
     @classmethod
     def get(cls):
@@ -16,6 +19,7 @@ class all(Resource):
 
         return cityjson_list_schema.dump(all)
 
+##Get all the information of an object, given a certain value: So for instance, select building with object_id NL.IMBAG.Pand.0518100000213709-0, or select building with id 2
 class QueryByAttribute(Resource):
     @classmethod
     def get(cls, attrib: str, value: str):
@@ -26,6 +30,7 @@ class QueryByAttribute(Resource):
 
         return cityjson_list_schema.dump(cj_object)
 
+## Get the attribute info of an object, given the object_id, so for instance, get the children, parents, or attributes of NL.IMBAG.Pand.0518100000213709-0
 class GetInfo(Resource):
     @classmethod
     def get(cls, attrib: str, object_id: str):
@@ -36,6 +41,19 @@ class GetInfo(Resource):
             return {"message": "Object not found"}, 404
 
         return attribute
+
+class FilterAttributes(Resource):
+    @classmethod
+    def get(cls, attrib: str, operator:str, value: str):
+        if operator == "smaller":
+            cj_object = session.query(CjObjectModel).filter(CjObjectModel.attributes[attrib] < value).all()
+        if operator == "bigger":
+            cj_object = session.query(CjObjectModel).filter(CjObjectModel.attributes[attrib] > value).all()
+
+        if not cj_object:
+            return {"message": "Object not found"}, 404
+
+        return cityjson_list_schema.dump(cj_object)
 
 
 ##in progress
@@ -69,10 +87,30 @@ class AddAttribute(Resource):
 
         return cityjson_list_schema.dump(objects)
 
+#attempt to use raw sequel
+# class Sequel(Resource):
+#     @classmethod
+#     def get(cls):
+#         objects = session.query(CjObjectModel).all()
+#
+#         sql = text(("UPDATE objects SET attributes = jsonb_set(attributes::jsonb, ’{roof_area}’, ’123’)::json WHERE type = ’Building’;"))
+#
+#
+#         for object in objects:
+#             # session.execute(sql, **object)
+#             session.execute(("UPDATE objects SET attributes = jsonb_set(attributes::jsonb, ’{roof_area}’, ’123’)::json WHERE type = ’Building’;"))
+#         # objects.session.exectute("UPDATE objects SET attributes = jsonb_set(attributes::jsonb, ’{roof_area}’, ’123’)::json WHERE type = ’Building’;")
+#         # for object in objects:
+#         #     listObj = object.attributes
+#         #     listObj["test_attribute"] = "test_value"
+#         #     object.attributes = listObj
+#         #     session.commit()  # This is supposed to save the changes to the DB, but it doesn't
+#
+#         return cityjson_list_schema.dump(objects)
+
 
 #todo
-class FilterAttributes(Resource):
-    pass
+
 
 class CalculateVolume (Resource):
     pass
@@ -80,5 +118,22 @@ class CalculateVolume (Resource):
 class SomethingWithGeometry(Resource):
     pass
 
-class Sequel(Resource):
-    pass
+# class Sequel(Resource):
+#     @classmethod
+#     def get(cls):
+#         objects = session.query(CjObjectModel).all()
+#
+#         sql = text(("UPDATE objects SET attributes = jsonb_set(attributes::jsonb, ’{roof_area}’, ’123’)::json WHERE type = ’Building’;"))
+#
+#
+#         for object in objects:
+#             # session.execute(sql, **object)
+#             session.execute(("UPDATE objects SET attributes = jsonb_set(attributes::jsonb, ’{roof_area}’, ’123’)::json WHERE type = ’Building’;"))
+#         # objects.session.exectute("UPDATE objects SET attributes = jsonb_set(attributes::jsonb, ’{roof_area}’, ’123’)::json WHERE type = ’Building’;")
+#         # for object in objects:
+#         #     listObj = object.attributes
+#         #     listObj["test_attribute"] = "test_value"
+#         #     object.attributes = listObj
+#         #     session.commit()  # This is supposed to save the changes to the DB, but it doesn't
+#
+#         return cityjson_list_schema.dump(objects)
--- cjdb_api/app/routes/__init__.py
+++ cjdb_api/app/routes/__init__.py
@@ -17,5 +17,7 @@ api.add_resource(query.GetInfo, "/info/<string:attrib>/<string:object_id>")
 api.add_resource(query.CalculateFootprint, "/area/<string:object_id>")
 api.add_resource(query.CalculateVolume, "/volume/<string:object_id>")
 api.add_resource(query.AddAttribute, "/update")
+api.add_resource(query.FilterAttributes, "/filter/<string:attrib>/<string:operator>/<string:value>")
+# api.add_resource(query.Sequel, "/sql")
 
 # todo add more resources when they are ready
