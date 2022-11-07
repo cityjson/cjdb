88d8d264427a29817d0e64e30bdfd623667691c2
leonpowalka <leon.powalka@geodan.nl>
2022-11-07 21:52:53 +0100

    cjdb_api arg_parser argument groups

--- cjdb_api/app/arg_parser.py
+++ cjdb_api/app/arg_parser.py
@@ -4,20 +4,22 @@ import os
 def Parser():
     parser = argparse.ArgumentParser(description='Run cjdb_api')
 
-    parser.add_argument('-C', '--conn-string', type=str, required=True,
+    required = parser.add_argument_group(title="Required arguments")
+    required.add_argument('-C', '--conn-string', type=str, required=True,
                 default=os.getenv("CJDB_CONN_STRING"),
                 help="Database connection string in the form of: " + \
                 "postgresql://<user>:<password>@<host>:<port>/<database>", 
                 dest="conn_string")
 
-    parser.add_argument('-p', '--port', type=int, default=8080,
+    optional = parser.add_argument_group(title="Optional arguments")
+    optional.add_argument('-p', '--port', type=int, default=8080,
                         help="Web application port", dest="port")
 
-    parser.add_argument('-s', '--schema', type=str, default='public',
+    optional.add_argument('-s', '--schema', type=str, default='public',
                         help="Database schema, where the CityObjects are kept.", 
                         dest="db_schema")
 
-    parser.add_argument('-d', '--debug', default=False,
+    optional.add_argument('-d', '--debug', default=False,
                 action='store_const', const=True,
                 help="Run in debug mode.",
                 dest="debug_mode")
