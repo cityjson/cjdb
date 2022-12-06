eedc557029d0bc8c0181e0eef97993f6978c0628
Cynthia Cai <81803109+cynthiacai56@users.noreply.github.com>
2022-12-06 16:07:54 +0100

    Add new blueprint

--- cjdb_api/app/__init__.py
+++ cjdb_api/app/__init__.py
@@ -2,6 +2,7 @@ from cjdb_api.app.vars import DEBUG, CONN_STRING
 from cjdb_api.app.db import db
 from cjdb_api.app.ma import ma
 from cjdb_api.app.routes import api_blueprint
+from cjdb_api.app.routes.v1 import v1_api_blueprint
 from flask import Flask
 
 
@@ -14,8 +15,7 @@ def make_app():
     db.init_app(flask_app)
     ma.init_app(flask_app)
 
-    flask_app.register_blueprint(api_blueprint, url_prefix="/api")
-
+    flask_app.register_blueprint(api_blueprint, url_prefix="/")
+    flask_app.register_blueprint(v1_api_blueprint, url_prefix="/v1")
+    
     return flask_app
-
-
