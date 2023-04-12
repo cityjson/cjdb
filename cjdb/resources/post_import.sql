44c87c86baa563032f13a9ba2adb7e4336512b37
Gina Stavropoulou <ginastavropoulou@gmail.com>
2023-04-12 15:06:25 +0300

    sql fix

--- cjdb/resources/post_import.sql
+++ cjdb/resources/post_import.sql
@@ -1,21 +1,21 @@
 -- import_meta indexes
-CREATE INDEX IF NOT EXISTS import_meta_gix ON { schema }.import_meta USING gist(bbox);
+CREATE INDEX IF NOT EXISTS import_meta_gix ON {schema}.import_meta USING gist(bbox);
 
-CREATE INDEX IF NOT EXISTS import_meta_source_file_idx ON { schema }.import_meta USING hash(source_file);
+CREATE INDEX IF NOT EXISTS import_meta_source_file_idx ON {schema}.import_meta USING hash(source_file);
 
 -- cj_object indexes
-CREATE INDEX IF NOT EXISTS cj_object_type_idx ON { schema }.cj_object USING btree("type");
+CREATE INDEX IF NOT EXISTS cj_object_type_idx ON {schema}.cj_object USING btree("type");
 
-CREATE INDEX IF NOT EXISTS cj_object_ground_gix ON { schema }.cj_object USING gist(ground_geometry);
+CREATE INDEX IF NOT EXISTS cj_object_ground_gix ON {schema}.cj_object USING gist(ground_geometry);
 
-CREATE INDEX lod ON { schema }.cj_object USING gin (geometry);
+CREATE INDEX lod ON {schema}.cj_object USING gin (geometry);
 
 -- family indexes
-CREATE INDEX IF NOT EXISTS family_parent_idx ON { schema }.family USING btree(parent_id);
+CREATE INDEX IF NOT EXISTS family_parent_idx ON {schema}.family USING btree(parent_id);
 
-CREATE INDEX IF NOT EXISTS family_child_idx ON { schema }.family USING btree(child_id);
+CREATE INDEX IF NOT EXISTS family_child_idx ON {schema}.family USING btree(child_id);
 
 -- clustering
-cluster { schema }.import_meta USING import_meta_gix;
+cluster {schema}.import_meta USING import_meta_gix;
 
-cluster { schema }.cj_object USING cj_object_ground_gix;
+cluster {schema}.cj_object USING cj_object_ground_gix;
