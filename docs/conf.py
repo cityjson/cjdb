deefb0989a36c67e5b53090b616c3c5fec063216
Gina Stavropoulou <ginastavropoulou@gmail.com>
2023-07-28 16:09:16 +0200

    added dosc

---
+++ docs/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+from cjdb import __version__
+
+sys.path.insert(0, os.path.abspath(".."))
+
+
+project = 'cjdb'
+copyright = '2023, Gina Stavropoulou'
+author = 'Gina Stavropoulou'
+release = __version__
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc", "sphinx_click"]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_theme_path = ["_themes"]
+html_static_path = ['_static']
